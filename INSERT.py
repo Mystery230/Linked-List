48d69435f0ba2c6d41e84e6882b42b6fc31c5a14
Mystery230 <mishbahulmuflihin@gmail.com>
2025-06-17 22:32:28 +0700

    Add files via upload

---
+++ INSERT.py
@@ -0,0 +1,88 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+
+my_linked_list = LinkedList(5)
+my_linked_list.append(10)
+my_linked_list.append(20)
+
+print('LL before insert():')
+my_linked_list.print_list()
+
+my_linked_list.insert(1, 15)
+my_linked_list.insert(0, 442023611074)
+
+print('\nLL after insert(2) in middle:')
+my_linked_list.print_list()
+
+my_linked_list.insert(0, 1)
+my_linked_list.insert(3, 442023611074)
+
+print('\nLL after insert(0) at beginning:')
+my_linked_list.print_list()
+
+my_linked_list.insert(5, 30)
+my_linked_list.insert(5, 442023611074)
+
+print('\nLL after insert(4) at end:')
+my_linked_list.print_list()
---
+++ SET.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+
+my_linked_list = LinkedList(5)
+my_linked_list.append(10)
+my_linked_list.append(20)
+my_linked_list.append(30)
+my_linked_list.append(40)
+my_linked_list.append(45)
+
+print('LL before set_value():')
+my_linked_list.print_list()
+
+my_linked_list.set_value(4, 50)
+
+print('\nLL after set_value():')
+my_linked_list.print_list()
